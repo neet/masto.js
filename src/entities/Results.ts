bc10d9c196a5340696b4cfbe3f0ff3fd7fce870d
neet <n33t5hin@gmail.com>
2018-09-02 10:18:01 +0900

    Use conditional type to simplify

--- src/entities/Results.ts
+++ src/entities/Results.ts
@@ -2,16 +2,13 @@ import { Account } from '@/entities/Account';
 import { Status } from '@/entities/Status';
 import { Tag } from '@/entities/Tag';
 
-interface HashtagsValueMap {
-  'v1': string[];
-  'v2': Tag[];
-}
-
-export interface Results<V extends keyof HashtagsValueMap = 'v2'> {
+export interface Results<V extends 'v1'|'v2'> {
   /** An array of matched Accounts */
   accounts: Account[];
   /** An array of matched Statuses */
   statuses: Status[];
   /** An array of matched hashtags, as strings */
-  hashtags: HashtagsValueMap[V];
+  hashtags: V extends 'v1' ? string[]
+          : V extends 'v2' ? Tag[]
+          : never;
 }
