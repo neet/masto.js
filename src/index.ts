419a2df71d69ad6ce01c418edb64740509ae0c5a
neet <n33t5hin@gmail.com>
2018-09-02 10:08:04 +0900

    Export error classes

--- src/index.ts
+++ src/index.ts
@@ -19,4 +19,10 @@ export * from './entities/Credentials';
 export * from './entities/OAuth';
 export * from './entities/PushSubscription';
 
+export * from './errors/MastodonError';
+export * from './errors/MastodonNotFoundError';
+export * from './errors/MastodonRatelimitError';
+export * from './errors/MastodonUnauthorizedError';
+export * from './errors/MastodonURLResolveError';
+
 export { Mastodon as default } from './client/Mastodon';
