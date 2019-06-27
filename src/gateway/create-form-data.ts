c32593700bc57e569a6c19f6e9b3a01f303999a7
Neetshin <n33t5hin@gmail.com>
2019-06-27 23:58:35 +0900

    Add comment

--- src/gateway/create-form-data.ts
+++ src/gateway/create-form-data.ts
@@ -6,6 +6,28 @@ export const isArray = (x: any): x is any[] =>
 export const isObject = (x: any): x is { [key: string]: any } =>
   Object.prototype.toString.call(x) === '[object Object]';
 
+/**
+ * Encode nested object to form-data compatible flat object
+ * @example
+ * const result = createFormData({
+ *   fruit: 'apple',
+ *   animals: ['dog', 'elephant'],
+ *   foo: {
+ *     foo1: {
+ *       foo11: 'bar'
+ *     }
+ *   }
+ * });
+ *
+ * result.get('a') === 'apple';
+ * result.getAll('b[]') === ['dog', 'elephant'];
+ * result.get('foo[foo1][foo11]') === 'bar';
+ *
+ * @param encodable Object to encode
+ * @param result Result value (for recursive function)
+ * @param parentKey Parent key (for recursive function)
+ * @return Encoded FormData
+ */
 export function createFormData(
   encodable: any,
   result = new FormData(),
