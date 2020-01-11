4b927055f66a1b4e4d92cff1edac22b13596a13a
Neetshin <n33t5hin@gmail.com>
2020-01-11 15:18:05 +0900

    Use @rollup packages

--- rollup.config.js
+++ rollup.config.js
@@ -1,36 +1,45 @@
-import { terser } from 'rollup-plugin-terser';
-import commonjs from 'rollup-plugin-commonjs';
-import json from 'rollup-plugin-json';
-import resolve from 'rollup-plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
+import resolve from '@rollup/plugin-node-resolve';
 import typescript from 'rollup-plugin-typescript2';
 import builtins from 'rollup-plugin-node-builtins';
-import autoExternal from 'rollup-plugin-auto-external';
+import { terser } from 'rollup-plugin-terser';
 import packageJSON from './package.json';
 
-const formatMap = {
-  main: 'cjs',
-  browser: 'umd',
-  module: 'esm',
-};
+// import autoExternal from 'rollup-plugin-auto-external';
 
-export default ['main', 'browser', 'module'].map(dist => ({
-  input: './src/index.ts',
-  output: {
-    name: packageJSON.name,
-    file: packageJSON[dist],
-    format: formatMap[dist],
-    exports: 'named',
+export default [
+  {
+    input: './src/index.ts',
+    output: {
+      file: packageJSON.main,
+      format: 'cjs',
+    },
+    plugins: [commonjs(), json(), typescript()],
   },
-  plugins: [
-    resolve({
-      preferBuiltins: true,
-      browser: dist === 'browser',
-    }),
-    dist === 'browser' && builtins(),
-    commonjs(),
-    json(),
-    typescript(),
-    terser(),
-    dist !== 'browser' && autoExternal(),
-  ],
-}));
+  {
+    input: './src/index.ts',
+    output: {
+      file: packageJSON.module,
+      format: 'esm',
+    },
+    plugins: [commonjs(), json(), typescript()],
+  },
+  {
+    input: './src/index.ts',
+    output: {
+      file: packageJSON.browser,
+      format: 'umd',
+      name: packageJSON.name,
+      exports: 'named',
+    },
+    plugins: [
+      resolve({ preferBuiltins: true }),
+      builtins(),
+      commonjs(),
+      json(),
+      typescript(),
+      terser(),
+    ],
+  },
+];
