e9e9d1d321bdaa985ddde858827753125b5f8396
Neetshin <n33t5hin@gmail.com>
2020-01-31 13:18:40 +0900

    Rename TagHistory to History

--- src/entities/tag.ts
+++ src/entities/tag.ts
@@ -1,17 +1,10 @@
+import { History } from './history';
+
 export interface Tag {
   /** The hashtag, not including the preceding `#` */
   name: string;
   /** The URL of the hashtag */
   url: string;
   /** Array of History */
-  history: TagHistory[];
-}
-
-export interface TagHistory {
-  /** String of UNIX timestamp */
-  day: string;
-  /** How many times was used */
-  uses: number;
-  /** How many unique users are there */
-  accounts: number;
+  history: History[];
 }
