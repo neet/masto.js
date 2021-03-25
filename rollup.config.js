c368011fd2264a70a25fe37beac82f93afe9f520
Ryo Igarashi <n33t5hin@gmail.com>
2021-03-25 22:52:18 +0900

    chore: Remove browser field

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,5 @@
 import commonjs from '@rollup/plugin-commonjs';
 import json from '@rollup/plugin-json';
-import resolve from '@rollup/plugin-node-resolve';
-import builtins from 'rollup-plugin-node-builtins';
-import { terser } from 'rollup-plugin-terser';
 import typescript from 'rollup-plugin-typescript2';
 
 import packageJSON from './package.json';
@@ -46,22 +43,5 @@ export default [
     },
     plugins: [commonjs(), json(), typescript()],
     external,
-  },
-  {
-    input: './src/index.ts',
-    output: {
-      file: packageJSON.browser,
-      format: 'umd',
-      name: packageJSON.name,
-      exports: 'named',
-    },
-    plugins: [
-      resolve({ preferBuiltins: true }),
-      builtins(),
-      commonjs(),
-      json(),
-      typescript(),
-      terser(),
-    ],
   },
 ];
