03625e6b2a3ce339db1fd864a4eb48b0e626daef
Ryo Igarashi <n33t5hin@gmail.com>
2021-03-25 22:01:12 +0900

    feat: Enable polling of media attachment by default

--- src/repositories/media-attachment-repository.ts
+++ src/repositories/media-attachment-repository.ts
@@ -13,6 +13,9 @@ export interface CreateMediaAttachmentParams {
   readonly focus?: string | null;
   /** Custom thumbnail */
   readonly thumbnail?: unknown | null;
+
+  /** Wait resolving promise for the media to be uploaded. Defaults to `false` */
+  readonly skipPolling?: boolean;
 }
 
 export type UpdateMediaAttachmentParams = Partial<CreateMediaAttachmentParams>;
@@ -27,61 +30,12 @@ export class MediaAttachmentRepository
   constructor(private readonly http: Http, readonly version: string) {}
 
   /**
-   * Creates an attachment to be used with a new status.
-   * @param params Parameters
-   * @return Attachment
-   * @see https://docs.joinmastodon.org/methods/statuses/media/
-   */
-  @deprecated('Use Masto.media#create instead')
-  @version({ since: '0.0.0', until: '3.1.3' })
-  v1__create(params: CreateMediaAttachmentParams) {
-    return this.http.post<Attachment>(`/api/v1/media`, params, {
-      headers: { 'Content-Type': 'multipart/form-data' },
-    });
-  }
-
-  /**
-   * Creates an attachment to be used with a new status.
-   * @param params Parameters
-   * @return Attachment
-   * @see https://docs.joinmastodon.org/methods/statuses/media/
-   */
-  @version({ since: '3.1.3' })
-  create(params: CreateMediaAttachmentParams) {
-    return this.http.post<Attachment>(`/api/v2/media`, params, {
-      headers: { 'Content-Type': 'multipart/form-data' },
-    });
-  }
-
-  /**
-   * Fetches an attachment to be used with a new status.
-   * @param id ID of the attachment
-   * @see https://github.com/tootsuite/mastodon/pull/13210
-   */
-  @version({ since: '3.1.3' })
-  fetch(id: string) {
-    return this.http.get<Attachment>(`/api/v1/media/${id}`);
-  }
-
-  /**
-   * Update an Attachment, before it is attached to a status and posted.
-   * @param id The id of the Attachment entity to be updated
-   * @param params Parameters
-   * @return Attachment
-   * @see https://docs.joinmastodon.org/api/rest/media/#put-api-v1-media-id
-   */
-  @version({ since: '0.0.0' })
-  update(id: string, params: UpdateMediaAttachmentParams): Promise<Attachment> {
-    return this.http.put(`/api/v1/media/${id}`, params);
-  }
-
-  /**
    * @experimental
    * @param id ID of the media
    * @param interval interval of polling
    * @returns Media attachment that has done processing
    */
-  async waitFor(id: string, interval = 1000) {
+  waitFor(id: string, interval = 1000): Promise<Attachment> {
     return timeout(
       (async () => {
         let media: Attachment | null = null;
@@ -100,5 +54,72 @@ export class MediaAttachmentRepository
       // TODO: fix
       3000,
     );
+  }
+
+  /**
+   * Creates an attachment to be used with a new status.
+   * @param params Parameters
+   * @return Attachment
+   * @see https://docs.joinmastodon.org/methods/statuses/media/
+   */
+  @version({ since: '3.1.3' })
+  async create({
+    skipPolling = false,
+    ...params
+  }: CreateMediaAttachmentParams): Promise<Attachment> {
+    const media = await this.http.post<Attachment>(`/api/v2/media`, params, {
+      headers: { 'Content-Type': 'multipart/form-data' },
+    });
+
+    if (skipPolling) return media;
+    return this.waitFor(media.id);
+  }
+
+  /**
+   * Fetches an attachment to be used with a new status.
+   * @param id ID of the attachment
+   * @see https://github.com/tootsuite/mastodon/pull/13210
+   */
+  @version({ since: '3.1.3' })
+  fetch(id: string) {
+    return this.http.get<Attachment>(`/api/v1/media/${id}`);
+  }
+
+  /**
+   * Update an Attachment, before it is attached to a status and posted.
+   * @param id The id of the Attachment entity to be updated
+   * @param params Parameters
+   * @return Attachment
+   * @see https://docs.joinmastodon.org/methods/statuses/media/
+   */
+  @version({ since: '0.0.0' })
+  async update(
+    id: string,
+    { skipPolling = false, ...params }: UpdateMediaAttachmentParams,
+  ): Promise<Attachment> {
+    const media = await this.http.put<Attachment>(
+      `/api/v1/media/${id}`,
+      params,
+      {
+        headers: { 'Content-Type': 'multipart/form-data' },
+      },
+    );
+
+    if (skipPolling) return media;
+    return this.waitFor(media.id);
+  }
+
+  /**
+   * Creates an attachment to be used with a new status.
+   * @param params Parameters
+   * @return Attachment
+   * @see https://docs.joinmastodon.org/methods/statuses/media/
+   */
+  @deprecated('Use Masto.media#create instead')
+  @version({ since: '0.0.0', until: '3.1.3' })
+  v1__create(params: CreateMediaAttachmentParams) {
+    return this.http.post<Attachment>(`/api/v1/media`, params, {
+      headers: { 'Content-Type': 'multipart/form-data' },
+    });
   }
 }
