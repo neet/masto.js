7f6b9bfe6aa8b1181e6f35e04368245fa4bbaa27
Ryo Igarashi <n33t5hin@gmail.com>
2021-09-11 09:22:48 +0900

    fix: Avoid unintuitive keyword like "facade"

--- src/admin.ts
+++ src/admin.ts
@@ -1,7 +1,7 @@
 import { Http } from './http';
 import { AdminRepositories } from './repositories';
 
-export class AdminFacadeRepositories {
+export class MastoAdminClient {
   constructor(private readonly http: Http, private readonly version: string) {}
 
   readonly account = new AdminRepositories.AccountRepository(
@@ -14,3 +14,8 @@ export class AdminFacadeRepositories {
     this.version,
   );
 }
+
+/**
+ * @deprecated This alias will be removed in v5.0.0
+ */
+export const AdminFacadeRepositories = MastoAdminClient;
