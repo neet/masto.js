89e00985885669e663d2d5461bf44e470827a6e9
Jackson Samuel <64137531+JacksonSamuel42@users.noreply.github.com>
2022-12-14 06:03:46 +0100

    feat: Support /v1/admin/ip_blocks (#752)
    * adding new v4 admin endpoit admin-ip-block
    
    * removing spaces

---
+++ src/repositories/admin/ip-block-repository.ts
@@ -0,0 +1,105 @@
+import type { MastoConfig } from '../../config';
+import { version } from '../../decorators';
+import type { Admin } from '../../entities';
+import type { IpBlockSeverity } from '../../entities/admin';
+import type { Http } from '../../http';
+import type { Repository } from '../repository';
+
+export type FetchIpBlockParams = {
+  /** Integer. Maximum number of results to return. Defaults to 100. */
+  readonly limit?: number | null;
+};
+
+export interface CreateIpBlockParams {
+  /** The IP address and prefix to block. */
+  readonly ip?: string;
+  /** The policy to apply to this IP range. */
+  readonly severity: IpBlockSeverity;
+  /** The reason for this IP block. */
+  readonly comment?: string;
+  /** The number of seconds in which this IP block will expire. */
+  readonly expiresIn?: number | null;
+}
+
+export interface UpdateIpBlockParams {
+  /** The IP address and prefix to block. */
+  readonly ip?: string;
+  /** The policy to apply to this IP range. */
+  readonly severity?: IpBlockSeverity;
+  /** The reason for this IP block. */
+  readonly comment?: string;
+  /** The number of seconds in which this IP block will expire. */
+  readonly expiresIn?: number | null;
+}
+
+export class IpBlockRepository
+  implements
+    Repository<
+      Admin.IpBlock,
+      CreateIpBlockParams,
+      UpdateIpBlockParams,
+      never,
+      FetchIpBlockParams
+    >
+{
+  constructor(
+    private readonly http: Http,
+    readonly version: string,
+    readonly config: MastoConfig,
+  ) {}
+
+  /**
+   * Show information about all blocked IP ranges.
+   * @param params Parameters
+   * @return Array of Ip Block
+   * @see https://docs.joinmastodon.org/methods/admin/
+   */
+  @version({ since: '4.0.0' })
+  fetchAll(params?: FetchIpBlockParams): Promise<Admin.IpBlock[]> {
+    return this.http.get('/api/v1/admin/ip_blocks', params);
+  }
+
+  /**
+   * Show information about all blocked IP ranges.
+   * @param id id of the Ip blocked
+   * @return object of Ip Block
+   * @see https://docs.joinmastodon.org/methods/admin/
+   */
+  @version({ since: '4.0.0' })
+  fetch(id: string): Promise<Admin.IpBlock> {
+    return this.http.get(`/api/v1/admin/ip_blocks/${id}`);
+  }
+
+  /**
+   * Add an IP address range to the list of IP blocks.
+   * @param params Parameters
+   * @return object of Ip Block
+   * @see https://docs.joinmastodon.org/methods/admin/
+   */
+  @version({ since: '4.0.0' })
+  create(params: CreateIpBlockParams): Promise<Admin.IpBlock> {
+    return this.http.post('/api/v1/admin/ip_blocks', params);
+  }
+
+  /**
+   * Change parameters for an existing IP block.
+   * @param params Parameters
+   * @return object of Ip Block
+   * @see https://docs.joinmastodon.org/methods/admin/
+   */
+  @version({ since: '4.0.0' })
+  update(params: UpdateIpBlockParams): Promise<Admin.IpBlock> {
+    return this.http.put('/api/v1/admin/ip_blocks', params);
+  }
+
+  /**
+   * Lift a block against an IP range.
+   * @param id id of ip block
+   * @return null
+   * @see https://docs.joinmastodon.org/methods/admin/
+   */
+  @version({ since: '4.0.0' })
+  remove(id: string): Promise<void> {
+    return this.http.delete(`/api/v1/admin/ip_blocks/${id}`);
+  }
+}
