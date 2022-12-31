e563327ca67557be2325eb996ed907c13df2af6b
Ryo Igarashi <n33t5hin@gmail.com>
2022-12-31 09:48:42 +0900

    feat: Support admin dashboard API

--- src/mastodon/v1/entities/admin/index.ts
+++ src/mastodon/v1/entities/admin/index.ts
@@ -1,10 +1,11 @@
-export type { AccountRole, Account } from './account';
-export type { Report } from './report';
-export type { DomainBlock, DomainBlockSeverity } from './domain-block';
-export type { DomainAllow } from './domain-allow';
-export type {
-  EmailDomainBlock,
-  EmailDomainBlockHistory,
-} from './domain-email-block';
-export type { IpBlock, IpBlockSeverity } from './ip-block';
-export type { CanonicalEmailBlock } from './canonical-email-block';
+export * from './account';
+export * from './canonical-email-block';
+export * from './cohort';
+export * from './dimension';
+export * from './domain-allow';
+export * from './domain-block';
+export * from './domain-email-block';
+export * from './ip-block';
+export * from './measure';
+export * from './report';
+export * from './tag';
