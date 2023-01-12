c90871f7d21c157dcf31d1b5b2b2d97491f271e7
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2023-01-12 21:32:34 +0800

    fix: add missing fields for entity

--- src/mastodon/v1/entities/relationship.ts
+++ src/mastodon/v1/entities/relationship.ts
@@ -7,24 +7,28 @@ export interface Relationship {
   id: string;
   /** Are you following this user? */
   following: boolean;
-  /** Do you have a pending follow request for this user? */
-  requested: boolean;
-  /** Are you featuring this user on your profile? */
-  endorsed: boolean;
+  /** Are you receiving this user's boosts in your home timeline? */
+  showingReblogs: boolean;
+  /** Have you enabled notifications for this user? */
+  notifying: boolean;
+  /** Which languages are you following from this user? */
+  languages: string[];
   /** Are you followed by this user? */
   followedBy: boolean;
+  /** Are you blocking this user? */
+  blocking: boolean;
+  /** Is this user blocking you? */
+  blockedBy: boolean;
   /** Are you muting this user? */
   muting: boolean;
   /** Are you muting notifications from this user? */
   mutingNotifications: boolean;
-  /** Are you receiving this user's boosts in your home timeline? */
-  showingReblogs: boolean;
-  /** Are you blocking this user? */
-  blocking: boolean;
+  /** Do you have a pending follow request for this user? */
+  requested: boolean;
   /** Are you blocking this user's domain? */
   domainBlocking: boolean;
-  /** Is this user blocking you? */
-  blockedBy: boolean;
+  /** Are you featuring this user on your profile? */
+  endorsed: boolean;
   /** Personal note for this account */
   note?: string | null;
 }
