469e79f5477a1fd25d4035ab105e0d74d93c5724
Ryo Igarashi <n33t5hin@gmail.com>
2023-01-14 21:37:00 +0900

    fix: Fix isObject to work with Jest

--- src/serializers/is-object.ts
+++ src/serializers/is-object.ts
@@ -1,2 +1,2 @@
 export const isObject = (x: unknown): x is Record<string, unknown> =>
-  typeof x === 'object' && x !== null && x.constructor === Object;
+  typeof x === 'object' && x !== null && x.constructor.name === 'Object';
