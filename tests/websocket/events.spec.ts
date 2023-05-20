9207099b668aa5e45a5d31fd07a2e054b5e928bd
Ryo Igarashi <n33t5hin@gmail.com>
2023-05-20 18:19:41 +0900

    chore: Fix events test

--- tests/websocket/events.spec.ts
+++ tests/websocket/events.spec.ts
@@ -10,7 +10,7 @@ describe('events', () => {
       try {
         const events = session.ws.subscribe('public:local');
 
-        setImmediate(async () => {
+        setTimeout(async () => {
           const status = await session.rest.v1.statuses.create({
             status: 'test',
           });
@@ -21,7 +21,7 @@ describe('events', () => {
           });
           await delay(1000);
           await session.rest.v1.statuses.select(status.id).remove();
-        });
+        }, 500);
 
         const [e1, e2, e3] = await events.take(3).toArray();
 
@@ -43,14 +43,14 @@ describe('events', () => {
       try {
         const events = session.ws.subscribe('user');
 
-        setImmediate(async () => {
+        setTimeout(async () => {
           const filter = await session.rest.v2.filters.create({
             title: 'test',
             context: ['public'],
             keywordsAttributes: [{ keyword: 'TypeScript' }],
           });
           await session.rest.v2.filters.select(filter.id).remove();
-        });
+        }, 500);
 
         const [e] = await events.take(1).toArray();
 
@@ -70,9 +70,9 @@ describe('events', () => {
       try {
         const events = alice.ws.subscribe('user:notification');
 
-        setImmediate(async () => {
+        setTimeout(async () => {
           await bob.rest.v1.accounts.select(alice.id).follow();
-        });
+        }, 500);
 
         const [e] = await events.take(1).toArray();
         assert(e?.event === 'notification');
@@ -91,14 +91,14 @@ describe('events', () => {
       let id!: string;
 
       try {
-        setImmediate(async () => {
+        setTimeout(async () => {
           const status = await bob.rest.v1.statuses.create({
             status: `@${alice.acct} Hello there`,
             visibility: 'direct',
           });
           id = status.id;
           await delay(1000);
-        });
+        }, 500);
 
         const events = alice.ws.subscribe('direct');
         const [e] = await events.take(1).toArray();
