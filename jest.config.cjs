ac89a3ffd9f680bdfbcca019e72f1643c4d5931f
Ryo Igarashi <n33t5hin@gmail.com>
2023-07-03 17:03:31 +0900

    chore(test): Override tsconfig in jest.config

--- jest.config.cjs
+++ jest.config.cjs
@@ -11,7 +11,17 @@ module.exports = {
       displayName: 'e2e',
       testEnvironment: './test-utils/jest-environment.ts',
       testMatch: ['<rootDir>/tests/**/*.spec.ts'],
-      transform: { '^.+\\.tsx?$': 'ts-jest' },
+      transform: {
+        '^.+\\.tsx?$': [
+          'ts-jest',
+          {
+            tsconfig: {
+              target: 'esnext',
+              module: 'esnext',
+            },
+          },
+        ],
+      },
       globalSetup: '<rootDir>/test-utils/jest-global-setup.ts',
       setupFilesAfterEnv: ['<rootDir>/test-utils/jest-setup-after-env.ts'],
     },
